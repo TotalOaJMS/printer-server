1a61a8e689de03a2faa76e0c08e9ea150421d91b
TotalOaJMS <sungchul1206@gmail.com>
2026-04-30 00:28:32 +0900

    main.py

--- main.py
+++ main.py
@@ -172,7 +172,10 @@ def dashboard():
                             let wcell = row.insertCell();
                                 if (waste == 1) {wcell.innerText = "교체 필요"; wcell.style.color = "red";} 
                                     else {wcell.innerText = "정상";}
-                        row.insertCell().innerText = date.toLocaleString('ko-KR');
+                        let date = new Date(p.updated_at);
+                            // 한국시간으로 변환 (+9시간)
+                            date.setHours(date.getHours() + 9);
+                                row.insertCell().innerText = date.toLocaleString('ko-KR', { hour12: false });
                     });
                 });
         </script>
